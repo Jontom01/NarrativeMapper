04576073ef6b9c45b643f1494c4bce7ea37ea9cf
Jontom01 <jonnyy.thompson@gmail.com>
2025-04-03 09:22:34 -0400

    ok now i added tempcoderunner file and __pycache__

--- narrative_analyzer/tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-    embeddings_dict = batch_embeddings("comment_data_politics_1200.csv")
-
-    df = cluster_embeddings(embeddings_dict)
-
-    keywords_df = get_keywords(df)
-
-    sentiment_df = get_sentiment(keywords_df)
