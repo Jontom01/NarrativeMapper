bea007da6f673f4c283682b8a9e30f879f01f34d
Jontom01 <jonnyy.thompson@gmail.com>
2025-04-04 00:25:45 -0400

    fin

--- test_package.py
+++ test_package.py
@@ -3,7 +3,8 @@ from narrative_mapper import *
 #PACKAGE TEST USE
 
 if __name__ == "__main__":
-    '''
+    #Function Use Version
+    
 
     embeddings_dict = get_embeddings("unrelated_to_package/comment_data/comment_data_antiwork_1800.csv")
 
@@ -22,20 +23,19 @@ if __name__ == "__main__":
     csv2_df.to_csv("test_2.csv", index=False)
 
     print(fin)
+    
+    #OO Version
     '''
-    # Create an instance for a given community, e.g., r/antiwork
     mapper = NarrativeMapper("r/antiwork")
 
-    # Run the full pipeline on your CSV file:
     mapper.load_embeddings("unrelated_to_package/comment_data/comment_data_antiwork_1800.csv").cluster(n_components=20, n_neighbors=20, min_cluster_size=40, min_samples=15).summarize()
 
-    # Get outputs in various formats:
-    df_text = mapper.format_by_text()      # DataFrame of individual comments
-    df_cluster = mapper.format_by_cluster()  # DataFrame summarizing clusters
-    dict_output = mapper.format_to_dict()    # Dictionary summary
+    df_text = mapper.format_by_text()
+    df_cluster = mapper.format_by_cluster()
+    dict_output = mapper.format_to_dict()
 
     df_text.to_csv("test_1.csv", index=False)
     df_cluster.to_csv("test_2.csv", index=False)
 
     print(dict_output)
-
+'''
